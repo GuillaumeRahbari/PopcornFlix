6821bb1f04f5130b230774b1e7817a6971ca76f8
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-09-26 12:33:12 +0200

    Initialisation de la partie web

---
+++ WebApp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name popcornFlixApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the popcornFlixApp
+ */
+angular.module('popcornFlixApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ WebApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name popcornFlixApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the popcornFlixApp
+ */
+angular.module('popcornFlixApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
