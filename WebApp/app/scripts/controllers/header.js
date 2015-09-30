3f02ee8cdb4ea892ff27e7fa526274f631e4ea57
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-09-30 21:22:02 +0200

    Ajout du contrôleur pour le header

---
+++ WebApp/app/scripts/controllers/header.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name popcornFlixApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the popcornFlixApp
+ */
+angular.module('popcornFlixApp')
+  .controller('HeaderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
