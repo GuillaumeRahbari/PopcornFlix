eaacf9796d1281086f780e8b7428204ef8b3cc8a
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-02 10:10:23 +0200

    On peut récupérer une image !

--- Backend/app.js
+++ Backend/app.js
@@ -41,37 +41,6 @@ app.options('/*', function(req, res){
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 /**
  * Permet de créer un serveur qui écoute sur le port 3000.
  * @type {http.Server}
--- Backend/routes/Movies.js
+++ Backend/routes/Movies.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var movieList = require(__dirname + '/Movies.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(movieList);
+});
+
+router.get('/images/:name', function(req, res, next) {
+    var img = fs.readFileSync(__dirname + '/../images/' + req.params.name);
+    res.writeHead(200, {'Content-Type': 'image/jpeg' });
+    res.end(img, 'binary');
 });
 
 module.exports = router;
