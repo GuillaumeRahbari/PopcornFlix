18360f03d8c71af5736e2b17c57358154830ee69
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-09 10:46:11 +0200

    Affiche un film

--- WebApp/app/scripts/directives/movie.js
+++ WebApp/app/scripts/directives/movie.js
@@ -7,12 +7,22 @@
  * # movie
  */
 angular.module('popcornFlixApp')
-  .directive('movie', function () {
+  .directive('movie', ['movieService',function (movieService) {
     return {
-      template: '<div></div>',
+      template:
+      '<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12">' +
+        '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">' +
+          '<img lazyload ll-src="http://localhost:3000/images/{{movie.image}}"/>' +
+        '</div>' +
+        '<div>' +
+          '{{movie.title}}' +
+        '</div>' +
+      '</div>',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the movie directive');
-      }
+      replace: true,
+      scope : {
+        movie: '=movie'
+      },
+      require: 'lazyload'
     };
-  });
+  }]);
