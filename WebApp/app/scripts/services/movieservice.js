ccc710acd744ddbb2d0bacd4cac363d5f7d6c632
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-09 10:46:30 +0200

    Suppression d'un truc inutile

--- WebApp/app/scripts/services/movieservice.js
+++ WebApp/app/scripts/services/movieservice.js
@@ -2,7 +2,7 @@
 
 /**
  * Ce service permet d'interroger le serveur sur tout ce qui concerne les films.
- * On peut notamment récupérer la liste des films, et récupérer l'image d'un film.
+ * On peut notamment récupérer la liste des films.
  * Pour cela il faut inclure le service <i>movieService</i>
  *
  * @example
@@ -60,50 +60,6 @@ angular.module('popcornFlixApp')
         $http({
           method : 'GET',
           url : constants.backendUrl + 'movies/',
-          headers: {'Content-Type': 'application/json'}
-        }).then(
-          function (data) { // success de node js
-            if (data.status === 200 ) { // success de la bdd
-              deferred.resolve(data.data);
-            }
-            else { // erreur de la bdd
-              deferred.reject('Erreur BDD : ' + data.status);
-            }
-          },
-          function () { // erreur de node js.
-            deferred.reject('Erreur de connexion !');
-          }
-        );
-        return deferred.promise;
-      },
-
-      /**
-       * Cette fonction permet de définir la liste des magasins choisis et de les enregistrer en base de données.
-       * Cette fonction retourne une promesse.
-       *
-       * @example Voici comment utiliser cette fonction :
-       *  var shopsChosenArray = [ 1, 2, 3]
-       *  shops.postShopsChosen(shopsChosenArray).then(
-       *    // Fonction callback lors du success
-       *    function (data) {
-       *      console.log(data);
-       *    },
-       *    // Fonction callback d'erreur.
-       *    function (msgErreur) {
-       *      console.log(msgErreur);
-       *    }
-       *  );
-       *
-       * @param {Object} data - Un tableau contenant la liste des magasins choisis (leur id).
-       * @returns {Function|promise} Retourne une promesse de réponse.
-       */
-      // TODO : vérifier que la liste envoyée est bien une liste ne contenant que des integer pour les ids.
-      postShopsChosen : function (data) {
-        var deferred = $q.defer();
-        $http({
-          method: 'POST',
-          url: constants.backendUrl + 'users/',
-          data: data,
           headers: {'Content-Type': 'application/json'}
         }).then(
           function (data) { // success de node js
