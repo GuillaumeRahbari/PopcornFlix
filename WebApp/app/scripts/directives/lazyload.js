b37cc26bf5a09b0d39ce2c4f77671acccb4c02c6
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-09 10:45:47 +0200

    Permet de charger les images en décaler

---
+++ WebApp/app/scripts/directives/lazyload.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name popcornFlixApp.directive:lazyload
+ * @description
+ * # lazyload
+ */
+angular.module('popcornFlixApp')
+  .directive('lazyload', ['$timeout',function ($timeout) {
+    return {
+      restrict:'A',
+      scope: {},
+      link: function(scope, elem, attrs) {
+        $timeout(function(){ elem.attr('src', attrs.llSrc) });
+      }
+    };
+  }]);
