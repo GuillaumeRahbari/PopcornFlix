0ba8c3ff82f5af9b8190e9794ccb3198bfdac7ac
grahbari <guillaume.rahbari@amadeus.com>
2015-10-21 17:13:14 +0200

    bar de recherche qui ne recherche rien pour l instant

--- WebApp/app/scripts/controllers/header.js
+++ WebApp/app/scripts/controllers/header.js
@@ -8,10 +8,6 @@
  * Controller of the popcornFlixApp
  */
 angular.module('popcornFlixApp')
-  .controller('HeaderCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('HeaderCtrl',[ function () {
+
+  }]);
